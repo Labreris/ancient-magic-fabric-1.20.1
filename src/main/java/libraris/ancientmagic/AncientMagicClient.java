9721abe5357984a1ecf1321331e74df477b6ac63
Labreris <secondarydisease@gmail.com>
2024-12-22 20:41:07 +0100

    2nd commit from intellij test

---
+++ src/main/java/libraris/ancientmagic/AncientMagicClient.java
@@ -0,0 +1,10 @@
+package libraris.ancientmagic;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AncientMagicClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/libraris/ancientmagic/AncientMagicDataGenerator.java
@@ -0,0 +1,11 @@
+package libraris.ancientmagic;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AncientMagicDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
